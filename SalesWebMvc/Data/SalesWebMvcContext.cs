c9a547937d5fc56f6b0219e27bf5a24770d472e9
Paulo Victor <59893766+pauloprojject@users.noreply.github.com>
2021-04-26 09:49:52 -0300

    Aula 7

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
 
 
 namespace SalesWebMvc.Data
@@ -14,8 +15,8 @@ namespace SalesWebMvc.Data
         {
         }
 
-        public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
-        public DbSet<SalesWebMvc.Models.Seller> Seller { get; set; }
-        public DbSet<SalesWebMvc.Models.SalesRecord> SalesRecord { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
