3ff4c746a781fd234b9df0d0dcdea1e1406b648a
Paulo Victor <59893766+pauloprojject@users.noreply.github.com>
2021-04-26 10:54:35 -0300

    Aula 9

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -11,7 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
-
+using SalesWebMvc.Services;
 namespace SalesWebMvc
 {
     public class Startup
@@ -40,6 +40,7 @@ namespace SalesWebMvc
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), buider => buider.MigrationsAssembly("SalesWebMvc")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
